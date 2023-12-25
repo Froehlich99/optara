e54e611cf83b2dca3622cec06cd07f5507f688dd
I569362 <maxim.froehlich@sap.com>
2023-12-25 15:12:54 +0100

    init

---
+++ constants/index.ts
@@ -0,0 +1,34 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Home" },
+  { href: "/", key: "dashboard", label: "Dashboard" },
+  { href: "/", key: "analysis", label: "Analysis" },
+  { href: "/", key: "Settings ", label: "Settings " },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      "About Hilink",
+      "Press Releases",
+      "Environment",
+      "Jobs",
+      "Privacy Policy",
+      "Contact Us",
+    ],
+  },
+  {
+    title: "Our Community",
+    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Admin Officer", value: "123-456-7890" },
+    { label: "Email Officer", value: "hilink@akinthil.com" },
+  ],
+};
