9ddb6981c83ecc50562eefc6255795227d639245
I569362 <maxim.froehlich@sap.com>
2023-12-28 13:02:27 +0100

    fix: fix button jumping

--- components/PopupMenu.tsx
+++ components/PopupMenu.tsx
@@ -24,35 +24,24 @@ const PopupMenu = ({
   NAV_LINKS,
 }: PopupMenuProps) => {
   return (
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          initial={{ y: 0 }}
-          animate={{ y: 0 }}
-          exit={{ y: 0 }}
-          transition={{
-            duration: 0.2,
-          }}
-        >
-          <ul
-            ref={refs.setFloating}
-            style={floatingStyles}
-            className="text-left absolute bg-white w-full border-b border-gray-300 flex flex-col z-20"
+    isOpen && (
+      <ul
+        ref={refs.setFloating}
+        style={floatingStyles}
+        className="text-left absolute bg-white w-full border-b border-gray-300 flex flex-col z-20"
+      >
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            onClick={() => setisOpen(false)}
+            className="hover:bg-gray-300 regular-16 py-2 px-5 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
           >
-            {NAV_LINKS.map((link) => (
-              <Link
-                href={link.href}
-                key={link.key}
-                onClick={() => setisOpen(false)}
-                className="hover:bg-gray-300 regular-16 py-2 px-5 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
-              >
-                {link.label}
-              </Link>
-            ))}
-          </ul>
-        </motion.div>
-      )}
-    </AnimatePresence>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    )
   );
 };
 
--- components/SearchPopup.tsx
+++ components/SearchPopup.tsx
@@ -1,4 +1,5 @@
 import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
 
 // Define the props for SearchPopup
 type SearchPopupProps = {
@@ -26,15 +27,23 @@ const SearchPopup: React.FC<SearchPopupProps> = ({
         className="absolute border border-gray-300 mt-5 bg-white rounded-lg"
       >
         <div className="max-container relative flex flex-col ">
-          <h1 className="px-5 py-5 bold-20">Popular Stocks</h1>
+          <h1 className="px-5 py-5 bold-20">
+            {stocks && stocks.length > 0 ? "Search Results" : "Popular Stocks"}
+          </h1>
           {stocks && stocks.length > 0 ? (
             stocks.map((stock: any, index: any) => (
-              <button
+              <Link
+                href={""}
                 key={index}
-                className="h-[3rem] bold-16 px-5 hover:bg-gray-300 text-start"
+                className="h-[3rem] bold-16 px-5 hover:bg-gray-300 text-start items-center"
               >
-                {stock.name}
-              </button>
+                <div className="flexBetween py-3">
+                  <div className="w-3/4 line-clamp-1">
+                    <p>{stock.name}</p>
+                  </div>
+                  <p>{stock.ticker}</p>
+                </div>
+              </Link>
             ))
           ) : (
             <>
