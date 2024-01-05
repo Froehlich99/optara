2d13cc06b7401587ee48d6382f803696bee672dc
I569362 <maxim.froehlich@sap.com>
2024-01-05 11:09:17 +0100

    feat: hide navbar on root

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,66 +9,71 @@ import PopupMenu from "./PopupMenu";
 import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
+  //TODO: Change to something less workaround-y
+  const showNavbar =
+    window.location.href === "http://localhost:3000/" ? false : true;
   const [isOpen, setisOpen] = useState(false);
   const { refs, floatingStyles } = useFloating();
 
   return (
-    <nav
-      ref={refs.setReference}
-      className="flexBetween max-container padding-container relative z-30 py-3"
-    >
-      <div className="flex flex-row items-center min-w-0 z-20">
-        <Link href={"/"}>
-          <Image
-            className="p-0"
-            src={"/cb-logo.svg"}
-            alt="logo"
-            width={60}
-            height={60}
-          ></Image>
-        </Link>
-        <div
-          className="min-w-0 px-3 w-[28rem] md:px-10"
-          onClick={() => setisOpen(false)}
-        >
-          <SearchBar />
-        </div>
-      </div>
-      <ul className="hidden h-full gap-12 md:flex items-center">
-        {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer transition-all hover:font-bold"
-          >
-            {link.label}
+    showNavbar && (
+      <nav
+        ref={refs.setReference}
+        className="flexBetween max-container padding-container relative z-30 py-3"
+      >
+        <div className="flex flex-row items-center min-w-0 z-20">
+          <Link href={"/"}>
+            <Image
+              className="p-0"
+              src={"/cb-logo.svg"}
+              alt="logo"
+              width={60}
+              height={60}
+            ></Image>
           </Link>
-        ))}
-        <UserButton afterSignOutUrl="/" />
-      </ul>
+          <div
+            className="min-w-0 px-3 w-[28rem] md:px-10"
+            onClick={() => setisOpen(false)}
+          >
+            <SearchBar />
+          </div>
+        </div>
+        <ul className="hidden h-full gap-12 md:flex items-center">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+          <UserButton afterSignOutUrl="/" />
+        </ul>
 
-      <Image
-        src={!isOpen ? "/menu.svg" : "/close.svg"}
-        alt="menu"
-        width={25}
-        height={20}
-        className="inline-block cursor-pointer md:hidden z-20"
-        onClick={() => setisOpen(!isOpen)}
-      />
-      <PopupMenu
-        isOpen={isOpen}
-        setisOpen={setisOpen}
-        refs={refs}
-        floatingStyles={floatingStyles}
-        NAV_LINKS={NAV_LINKS}
-      />
-      {isOpen && (
-        <div
-          className="fixed h-full w-full flex items-center justify-center top-0 left-0 z-10"
-          onClick={() => setisOpen(false)}
-        ></div>
-      )}
-    </nav>
+        <Image
+          src={!isOpen ? "/menu.svg" : "/close.svg"}
+          alt="menu"
+          width={25}
+          height={20}
+          className="inline-block cursor-pointer md:hidden z-20"
+          onClick={() => setisOpen(!isOpen)}
+        />
+        <PopupMenu
+          isOpen={isOpen}
+          setisOpen={setisOpen}
+          refs={refs}
+          floatingStyles={floatingStyles}
+          NAV_LINKS={NAV_LINKS}
+        />
+        {isOpen && (
+          <div
+            className="fixed h-full w-full flex items-center justify-center top-0 left-0 z-10"
+            onClick={() => setisOpen(false)}
+          ></div>
+        )}
+      </nav>
+    )
   );
 };
 
