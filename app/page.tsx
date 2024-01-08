70035eea8a4b5c1e0b097b40d2ca22b6dce9f49e
dev-lumos <70009384+dev-lumos@users.noreply.github.com>
2024-01-08 14:59:57 +0100

    Added new favicon and new logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import VideoComponent from "@/components/Video";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Main Page</main>;
+  return (
+    <main>
+      <VideoComponent />
+    </main>
+  );
 }
