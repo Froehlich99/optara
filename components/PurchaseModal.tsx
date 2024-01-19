c133480d5e3b281cc880afd4e0446e770ad505d5
Froehlich99 <maxim.froehlich4@gmail.com>
2024-01-19 11:42:40 +0100

    feat: add euro tab to purchaseModal

--- components/PurchaseModal.tsx
+++ components/PurchaseModal.tsx
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { Tab } from "@headlessui/react";
 import { formatCurrency } from "@/lib/utils";
 import { IUser } from "@/db/schema/User";
+import Image from "next/image";
 
 interface PurchaseModalProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
@@ -14,20 +15,52 @@ export const PurchaseModal: React.FC<PurchaseModalProps> = ({
   currentValue,
   user,
 }) => {
-  const [isQuantitySelected, setIsQuantitySelected] = useState(false);
+  const [isEuroSelected, setIsEuroSelected] = useState(false);
   const [quantity, setQuantity] = useState<number | null>(null);
+  const [amountEuro, setAmountEuro] = useState<number | null>(null);
   const [availableFunds, setAvailableFunds] = useState<number | null>(null);
+
+  const handleQuantityChange = (inputQuantity: number) => {
+    let maxAffordableQuantity;
+
+    if (currentValue) {
+      maxAffordableQuantity = Math.floor(user.money / currentValue);
+    } else {
+      maxAffordableQuantity = 0;
+    }
+
+    if (inputQuantity * (currentValue ? currentValue : 0) > user.money) {
+      setQuantity(maxAffordableQuantity);
+    } else {
+      setQuantity(inputQuantity);
+    }
+  };
+
+  const handleAmountEuroChange = (inputAmountEuro: number) => {
+    if (inputAmountEuro > user.money) {
+      inputAmountEuro = user.money;
+    }
+    setAmountEuro(inputAmountEuro);
+  };
 
   const handlePurchase = () => {
     let numPrice = currentValue;
-    let numQuantity = quantity;
-    if (!numQuantity || !numPrice || !availableFunds) return;
-    let totalPurchaseCost = numPrice * numQuantity;
-    if (totalPurchaseCost > availableFunds) {
+    let totalPurchaseCost;
+
+    if (isEuroSelected) {
+      if (!numPrice || !amountEuro) return;
+      totalPurchaseCost = amountEuro;
+    } else {
+      let numQuantity = quantity;
+      if (!numQuantity || !numPrice || !availableFunds) return;
+      totalPurchaseCost = numPrice * numQuantity;
+    }
+
+    if (totalPurchaseCost > user.money) {
       alert("Not enough funds");
       return;
     }
-    setAvailableFunds(availableFunds - totalPurchaseCost);
+    setAvailableFunds(user.money - totalPurchaseCost);
   };
   return (
     <div className="flex flex-col gap-5 h-full w-full">
@@ -36,70 +69,116 @@ export const PurchaseModal: React.FC<PurchaseModalProps> = ({
           <Tab.List className="">
             <Tab className="w-20">
               {({ selected }) => (
-                <button
+                <div
+                  role="button"
                   className={`p-2 rounded-l w-full  ${
                     selected
                       ? "bg-blue-500 text-white"
                       : "bg-gray-200 text-gray-800"
                   }`}
-                  onClick={() => setIsQuantitySelected(false)}
+                  onClick={() => setIsEuroSelected(false)}
                 >
-                  Euro
-                </button>
+                  Quantity
+                </div>
               )}
             </Tab>
             <Tab className="w-20">
               {({ selected }) => (
-                <button
+                <div
+                  role="button"
                   className={`flex p-2 rounded-r w-full justify-center  ${
                     selected
                       ? "bg-blue-500 text-white"
                       : "bg-gray-200 text-gray-800"
                   }`}
-                  onClick={() => setIsQuantitySelected(true)}
+                  onClick={() => setIsEuroSelected(true)}
                 >
-                  Quantity
-                </button>
+                  Euro
+                </div>
               )}
             </Tab>
           </Tab.List>
         </Tab.Group>
-        <button className="text-black" onClick={() => setIsOpen(false)}>
-          Close
-        </button>
-      </div>
-      <div></div>
-      <div className="inline-flex space-x-2 justify-between">
-        <p>Price: </p>
-        <p>{currentValue ? formatCurrency(currentValue) : "No Data"}</p>
-      </div>
-      <div className="inline-flex space-x-2 justify-between">
-        <p>Quantity: </p>
-        <input
-          className="block w-1/4 pe-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white outline-none text-right regular-16"
-          value={quantity ? quantity : ""}
-          type="number"
-          onChange={(e) => setQuantity(Number(e.target.value))}
+        <Image
+          src="/close.svg"
+          alt="close"
+          width={25}
+          height={20}
+          className="inline-block cursor-pointer z-20"
+          onClick={() => setIsOpen(false)}
         />
       </div>
-      <div className="inline-flex space-x-2 justify-between">
-        <p>Cost: </p>
-        <p>
-          {formatCurrency(
-            currentValue && quantity ? quantity * currentValue : 0
-          )}
-        </p>
-      </div>
-      <div className="inline-flex space-x-2 justify-between">
-        <p>Available Money: </p>
-        <p>
-          {formatCurrency(
-            currentValue && quantity
-              ? user.money - quantity * currentValue
-              : user.money
-          )}
-        </p>
-      </div>
+      {!isEuroSelected && (
+        <div className="flex flex-col gap-5">
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Price: </p>
+            <p>{currentValue ? formatCurrency(currentValue) : "No Data"}</p>
+          </div>
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Quantity: </p>
+            <input
+              className="block w-1/4 pe-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white outline-none text-right regular-16"
+              value={quantity ? quantity : ""}
+              type="number"
+              onChange={(e) => handleQuantityChange(Number(e.target.value))}
+            />
+          </div>
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Cost: </p>
+            <p>
+              {formatCurrency(
+                currentValue && quantity ? quantity * currentValue : 0
+              )}
+            </p>
+          </div>
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Available Money: </p>
+            <p>
+              {formatCurrency(
+                currentValue && quantity
+                  ? user.money - quantity * currentValue
+                  : user.money
+              )}
+            </p>
+          </div>
+        </div>
+      )}
+      {isEuroSelected && (
+        <div className="flex flex-col gap-5">
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Price: </p>
+            <p>{currentValue ? formatCurrency(currentValue) : "No Data"}</p>
+          </div>
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Amount: </p>
+            <input
+              className="block w-1/4 pe-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white outline-none text-right regular-16"
+              value={amountEuro ? amountEuro : ""}
+              type="number"
+              onChange={(e) => handleAmountEuroChange(Number(e.target.value))}
+            />
+          </div>
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Quantity: </p>
+            <p>
+              {currentValue && amountEuro
+                ? (amountEuro / currentValue).toFixed(2)
+                : 0}
+            </p>
+          </div>
+          <div className="inline-flex space-x-2 justify-between">
+            <p>Available Money: </p>
+            <p>
+              {formatCurrency(
+                currentValue && amountEuro
+                  ? user.money - amountEuro
+                  : user.money
+              )}
+            </p>
+          </div>
+        </div>
+      )}
+
       <div className="flex w-full justify-center mt-auto line-clamp-1 overflow-hidden">
         <button
           className="bg-green-90 px-8 py-4 text-white transition-all hover:bg-black flexCenter gap-3 rounded-full border w-full"
