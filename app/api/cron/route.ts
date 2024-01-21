32b7a6e5ef9c6d68bbd16a4554534958d600c333
Froehlich99 <maxim.froehlich4@gmail.com>
2024-01-21 10:46:20 +0100

    feat: add security to cron job

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -36,6 +36,12 @@ async function calculatePortfolio(user: IUser) {
 }
 
 export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
   try {
     // Find all users
     const users = await User.find({});
